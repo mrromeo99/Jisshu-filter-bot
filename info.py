6fec8e765f1a5c9607976e9dad97bd983725c2b1
mrromeo99 <montukaka818@gmail.com>
2025-04-21 18:50:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://iili.io/3zz4xIf.md.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://iili.io/3EtitJR.md.jpg https://iili.io/3EtiRtt.md.jpg https://iili.io/3EtPp6b.jpg https://iili.io/3EtPOyN.md.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
