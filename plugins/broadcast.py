190ea058c1e91dccce15f7bbde909e6fdfc8533a
mrromeo99 <montukaka818@gmail.com>
2025-05-19 20:15:19 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -3,102 +3,73 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import users_broadcast, groups_broadcast, temp, get_readable_time
+from utils import broadcast_messages, broadcast_messages_group
 import asyncio
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup 
 
 lock = asyncio.Lock()
 
-@Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
-async def broadcast_cancel(bot, query):
-    _, ident = query.data.split("#")
-    if ident == 'users':
-        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ᴜsᴇʀs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
-        temp.USERS_CANCEL = True
-    elif ident == 'groups':
-        temp.GROUPS_CANCEL = True
-        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ɢʀᴏᴜᴘs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
-       
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def users_broadcast(bot, message):
-    if lock.locked():
-        return await message.reply('Currently broadcast processing, Wait for complete.')
-
-    msg = await message.ask('<b>Do you want pin this message in users?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
-    if msg.text == 'Yes':
-        is_pin = True
-    elif msg.text == 'No':
-        is_pin = False
-    else:
-        return await msg.edit('Wrong Response!')
-    await msg.delete()
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ᴜsᴇʀs ⌛️</b>')
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    blocked = 0
-    deleted = 0
-    failed = 0
-    success = 0
-
-    async with lock:
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
+async def pm_broadcast(bot, message):
+    b_msg = await bot.ask(chat_id = message.from_user.id, text = "Nᴏᴡ Sᴇɴᴅ Mᴇ Yᴏᴜʀ Bʀᴏᴀᴅᴄᴀsᴛ Mᴇssᴀɢᴇ")
+    try:
+        users = await db.get_all_users()
+        sts = await message.reply_text('Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs...')
+        start_time = time.time()
+        total_users = await db.total_users_count()
+        done = 0
+        blocked = 0
+        deleted = 0
+        failed = 0
+        success = 0
         async for user in users:
-            time_taken = get_readable_time(time.time()-start_time)
-            if temp.USERS_CANCEL:
-                temp.USERS_CANCEL = False
-                await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
-                return
-            sts = await users_broadcast(int(user['id']), b_msg, is_pin)
-            if sts == 'Success':
-                success += 1
-            elif sts == 'Error':
+            if 'id' in user:
+                pti, sh = await broadcast_messages(int(user['id']), b_msg)
+                if pti:
+                    success += 1
+                elif pti == False:
+                    if sh == "Blocked":
+                        blocked += 1
+                    elif sh == "Deleted":
+                        deleted += 1
+                    elif sh == "Error":
+                        failed += 1
+                done += 1
+                if not done % 20:
+                    await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ ɪɴ ᴘʀᴏɢʀᴇss:\n\nTᴏᴛᴀʟ Usᴇʀs {total_users}\nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇss: {success}\nBʟᴏᴄᴋᴇᴅ: {blocked}\nDᴇʟᴇᴛᴇᴅ: {deleted}")    
+            else:
+                # Handle the case where 'id' key is missing in the user dictionary 
+                done += 1
                 failed += 1
-            done += 1
-            if not done % 20:
-                btn = [[
-                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
-                ]]
-                await b_sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
-        await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
-
+                if not done % 20:
+                    await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ ɪɴ ᴘʀᴏɢʀᴇss:\n\nTᴏᴛᴀʟ Usᴇʀs {total_users}\nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇss: {success}\nBʟᴏᴄᴋᴇᴅ: {blocked}\nDᴇʟᴇᴛᴇᴅ: {deleted}")    
+    
+        time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+        await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ Cᴏᴍᴘʟᴇᴛᴇᴅ:\nCᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ {time_taken} seconds.\n\nTᴏᴛᴀʟ Usᴇʀs: {total_users}\nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇss: {success}\nBʟᴏᴄᴋᴇᴅ: {blocked}\nDᴇʟᴇᴛᴇᴅ: {deleted}")
+    except Exception as e:
+        print(f"error: {e}")
         
-@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
-async def groups_broadcast(bot, message):
-    msg = await message.ask('<b>Do you want pin this message in groups?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
-    if msg.text == 'Yes':
-        is_pin = True
-    elif msg.text == 'No':
-        is_pin = False
-    else:
-        return await msg.edit('Wrong Response!')
-    await msg.delete()
-    chats = await db.get_all_chats()
-    b_msg = message.reply_to_message
-    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ɢʀᴏᴜᴘs ⏳</b>')
+@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS))
+async def broadcast_group(bot, message):
+    b_msg = await bot.ask(chat_id = message.from_user.id, text = "Nᴏᴡ Sᴇɴᴅ Mᴇ Yᴏᴜʀ Bʀᴏᴀᴅᴄᴀsᴛ Mᴇssᴀɢᴇ")
+    groups = await db.get_all_chats()
+    sts = await message.reply_text(
+        text='Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs Tᴏ Gʀᴏᴜᴘs...'
+    )
     start_time = time.time()
-    total_chats = await db.total_chat_count()
+    total_groups = await db.total_chat_count()
     done = 0
     failed = 0
+
     success = 0
-    
-    async with lock:
-        async for chat in chats:
-            time_taken = get_readable_time(time.time()-start_time)
-            if temp.GROUPS_CANCEL:
-                temp.GROUPS_CANCEL = False
-                await b_sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
-                return
-            sts = await groups_broadcast(int(chat['id']), b_msg, is_pin)
-            if sts == 'Success':
-                success += 1
-            elif sts == 'Error':
+    async for group in groups:
+        pti, sh = await broadcast_messages_group(int(group['id']), b_msg)
+        if pti:
+            success += 1
+        elif sh == "Error":
                 failed += 1
-            done += 1
-            if not done % 20:
-                btn = [[
-                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
-                ]]
-                await b_sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
-        await b_sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+        done += 1
+        if not done % 20:
+            await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ ɪɴ ᴘʀᴏɢʀᴇss:\n\nTᴏᴛᴀʟ Gʀᴏᴜᴘs {total_groups}\nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_groups}\nSᴜᴄᴄᴇss: {success}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ Cᴏᴍᴘʟᴇᴛᴇᴅ:\nCᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ {time_taken} sᴇᴄᴏɴᴅs.\n\nTᴏᴛᴀʟ Gʀᴏᴜᴘs {total_groups}\nCᴏᴍᴘʟᴇᴛᴇᴅ: {done} / {total_groups}\nSᴜᴄᴄᴇss: {success}")
+        
